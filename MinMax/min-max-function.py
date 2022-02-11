e322d83e6f1d04cff0f0eea6fdc57595bfd226fe
Nagmun Nahar Onu <62063972+Nagmun-Onu@users.noreply.github.com>
2022-02-11 14:56:25 +0100

    Update min-max-function.py

--- MinMax/min-max-function.py
+++ MinMax/min-max-function.py
@@ -1,26 +1,26 @@
 # @author: Nagmun Nahar Onu
 # version 1
-def minmax(sequence):
-  # set the first element of the sequence as minimum and maximum value
-    min = sequence[0]
-    max = sequence[0]
+def my_minmax(sequence):
+    # set the first element of the sequence as minimum and maximum value
+    my_min = sequence[0]
+    my_max = sequence[0]
     if len(sequence) > 1:
         for i in range(len(sequence)):
-            for j in range(i+1,len(sequence)):
-                if sequence[i] >= sequence [j]:
-                    if max < sequence[i]:
-                       max=sequence[i]
-                    if min > sequence[j]:
-                       min=sequence[j]
+            for j in range(i + 1, len(sequence)):
+                if sequence[i] >= sequence[j]:
+                    if my_max < sequence[i]:
+                        my_max = sequence[i]
+                    if my_min > sequence[j]:
+                        my_min = sequence[j]
                 else:
-                    if max < sequence[j]:
-                       max = sequence[j]
-                    if min > sequence[i]:
-                       min = sequence[i]
-    result = (min,max)
+                    if my_max < sequence[j]:
+                        my_max = sequence[j]
+                    if my_min > sequence[i]:
+                        my_min = sequence[i]
+    result = (my_min, my_max)
     print(result)
 
 # driver code
-minmax([1,2,3,5])  # expected output (1, 5)
-minmax([0,1,-2])   # expected output (-2, 1)
-minmax([3])        # expected output (3, 3)
+my_minmax([1,2,3,5])  # expected output (1, 5)
+my_minmax([0,1,-2])   # expected output (-2, 1)
+my_minmax([3])        # expected output (3, 3)
